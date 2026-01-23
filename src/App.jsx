881ad8bdb9f11a88b4f87e9274cac07e8e416264
eungyeolbaek <eungyeolbaek@gmail.com>
2026-01-23 16:50:04 +0900

    chore: 프로젝트 기초 설정 (reset/index CSS, 폰트 및 Vite 설정 복구)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import HabitPage from './domains/habit/habitPage';
-
-
-import './App.css'
+import styles from './App.module.css';
 
 function App() {
   return (
-    <main>
-      <HabitPage />
-
-    </main>
-    
-  )
-  
+    <>
+      {/* 추후 컴포넌트들이 들어올 예정입니다. */}
+      <h1 className={styles.srOnly}>공부의 숲</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
