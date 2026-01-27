44a0c17272344523c24f355757fd0355fffbeabb
ZoeYoon <bus1990@naver.com>
2026-01-27 21:47:48 +0900

    refactor: dailyHabit에서 habitList불러오는  useEffect를 부모인 habitPage 로 변경

--- src/domains/habit/HabitPage.jsx
+++ src/domains/habit/HabitPage.jsx
@@ -2,13 +2,41 @@ import { useEffect, useState } from 'react';
 import DailyHabit from './components/DailyHabit/DailyHabit';
 import styles from './HabitPage.module.css';
 import { LinkButton } from '@/components/LinkButton';
+import { fetchHabitList } from '@/apis/habit';
 
 
 function HabitPage({to, className}) {
-  //예비 id : studyid 번호
+  //예비 id : studyid 번호 각자의 seed 데이터의 studyId 값을 넣어주세요 
+  //이 부분은 추후 연결....
   const id= '101'
   const INTERVAL_TIME = 10000;
   const [current, setCurrent] = useState(new Date());
+
+
+  const [habitList, setHabitList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [studyName, setStudyName] = useState('')
+
+  //studyId => habit 가져오기 props로 dailyHabit에 내려줌 
+  // 여기서 가저온 studyName을 habitPage에서 사용.
+  useEffect(() => {
+    const dailyHabitlist = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const result = await fetchHabitList(id);
+        setHabitList(result.data.habits);
+        setStudyName(result.data.name);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  dailyHabitlist();
+  }, [id]);
+
 
   //날짜,시간 업로드
   useEffect(() => {
@@ -23,7 +51,7 @@ function HabitPage({to, className}) {
       <div className={styles.habitBox}>
         <div className={styles.navTop}>
           {/* 여기{study.name}으로 교체 */}
-          <p className={styles.studyNameTitle}>대나무의 늪</p>
+          <p className={styles.studyNameTitle}>{studyName}</p>
           <div className={styles.moveBtnContainer}>
           {/* 페이지 이동 연결 해야함  */}
           <LinkButton to={to} className={className}>오늘의 집중</LinkButton> 
@@ -40,7 +68,7 @@ function HabitPage({to, className}) {
         </div>
 
         {/* 임의로 id값 부여  */}
-        <DailyHabit id={id} />
+        <DailyHabit habitList={habitList} />
         {/* 넘어오지 마시오  */}
       </div>
     </div>
--- src/domains/habit/components/DailyHabit/DailyHabit.jsx
+++ src/domains/habit/components/DailyHabit/DailyHabit.jsx
@@ -1,56 +1,36 @@
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import styles from './DailyHabit.module.css';
-import { creatHabitCheckDate, deleteHabitCheckDate, fetchHabitList } from '@/apis/habit';
+import {
+  creatHabitCheckDate,
+  deleteHabitCheckDate,
+} from '@/apis/habit';
 import clsx from 'clsx';
 
-//habitPage에서 임의로 id값 부여...
-function DailyHabit({ id }) {
-  const [habitList, setHabitList] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-    //토글 state
+//부모인 habitPage에서 props로 habitList내려 받음 .
+function DailyHabit({ habitList }) {
   const [clickedHabitId, setClickedHabitId] = useState([]);
 
-
-  //habit 가져오기 
-  useEffect(() => {
-    const dailyHabitlist = async () => {
-      try {
-        setLoading(true);
-        setError(null);
-        const result = await fetchHabitList(id);
-        setHabitList(result.data.habits);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    dailyHabitlist();
-  }, [id]);
-
-
-  //토글 click 함수 
-  const handleClick = async(habitId) => {
+  //토글 click 함수
+  const handleClick = async (habitId) => {
     try {
       //현재 날짜 기준
       const checkDate = new Date().toISOString();
       const isChecked = clickedHabitId.includes(habitId);
-      
+
       if (isChecked) {
         await deleteHabitCheckDate(habitId, checkDate);
         console.log('삭제 성공');
       } else {
         await creatHabitCheckDate(habitId, checkDate);
         console.log('생성 성공');
-
       }
       setClickedHabitId((prev) =>
-        prev.includes(habitId)? prev.filter((id)=>id !== habitId): [...prev, habitId])
-
-
+        prev.includes(habitId)
+          ? prev.filter((id) => id !== habitId)
+          : [...prev, habitId],
+      );
     } catch (error) {
-      console.error('error:',error.message)
+      console.error('error:', error.message);
     }
   };
 
@@ -59,7 +39,6 @@ function DailyHabit({ id }) {
       <div className={styles.habitBox}>
         <h2 className={styles.title}>
           오늘의 습관
-
           {/* 목록 수정 링크 들어와야함 */}
           <button className={styles.patchHabitBtn}>목록 수정</button>
         </h2>
@@ -70,8 +49,10 @@ function DailyHabit({ id }) {
           {habitList.map((habit) => (
             <button
               key={habit.id}
-              className={clsx(styles.habitBtn, { [styles.habitBtnClick]: clickedHabitId.includes(habit.id) })}
-              onClick={()=>handleClick(habit.id)}
+              className={clsx(styles.habitBtn, {
+                [styles.habitBtnClick]: clickedHabitId.includes(habit.id),
+              })}
+              onClick={() => handleClick(habit.id)}
             >
               {habit.name}
             </button>
