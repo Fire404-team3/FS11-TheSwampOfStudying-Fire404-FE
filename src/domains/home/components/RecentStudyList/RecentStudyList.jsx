3fb5b46e2c4de0ea86aa1fe096fefc68012ed73c
DevWoojin97 <dnekdnwls@gmail.com>
2026-01-29 15:23:22 +0900

    최근 조회한 스터디 카드가 없을 때 최근 조회한 스터디 컨테이너 css가 올바르게 적용 안된 부분을 수정

--- src/domains/home/components/RecentStudyList/RecentStudyList.jsx
+++ src/domains/home/components/RecentStudyList/RecentStudyList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './RecentStudyList.module.css';
 import StudyCard from '../StudyCard';
+import clsx from 'clsx';
 export default function RecentStudyList() {
   const [recentStudies, setRecentStudies] = useState(() => {
     const saved = localStorage.getItem('recentStudies');
@@ -16,15 +17,21 @@ export default function RecentStudyList() {
     return [];
   });
 
-  if (recentStudies.length === 0) return <p>아직 조회한 스터디가 없어요</p>;
-
   return (
     <section className={styles.recentSection}>
       <h2 className={styles.title}>최근 조회한 스터디</h2>
-      <div className={styles.recentContainer}>
-        {recentStudies.map((study) => (
-          <StudyCard key={study.id} study={study} />
-        ))}
+      <div
+        className={clsx(styles.recentContainer, {
+          [styles.isEmpty]: recentStudies.length === 0,
+        })}
+      >
+        {recentStudies.length === 0 ? (
+          <p className={styles.emptyText}>아직 조회한 스터디가 없어요</p>
+        ) : (
+          recentStudies.map((study) => (
+            <StudyCard key={study.id} study={study} />
+          ))
+        )}
       </div>
     </section>
   );
